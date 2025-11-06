1c06a8faddbda61dda6da2468babcbe7e7522a72
houssemko <41808005+houssemko@users.noreply.github.com>
2025-11-06 13:38:45 +0100

    Add files via upload

---
+++ user.js
@@ -0,0 +1,25 @@
+/**********************************************************************
+ Recommended advanced configs                                                       
+**********************************************************************/
+
+/**Zen-UI***/
+user_pref("zen.view.experimental-force-window-controls-left", true);
+user_pref("zen.workspaces.show-workspace-indicator", false);
+user_pref("zen.view.grey-out-inactive-windows", false);
+user_pref("zen.theme.content-element-separation", 1);
+user_pref("zen.urlbar.show-protections-icon", true);
+user_pref("zen.view.hide-window-controls", false);
+user_pref("zen.theme.styled-status-panel", true);
+user_pref("zen.urlbar.replace-newtab", false);
+user_pref("zen.theme.accent-color", #3D3846);
+
+/** context-menu ***/
+user_pref("cmi-Hide-Inactive-Items-ContentAreaContextMenu", true);
+user_pref("cmi-Hide-Inactive-Items-TabContextMenu", true);
+user_pref("cmi-Center-Bookmark-Items", true);
+user_pref("cmi-fold_menu_item-enable", true);
+user_pref("cmi-Hide-Folder-Icon", false);
+user_pref("cmi-Auto-Hide-BookmarkBar", 1);
+user_pref("cmi-Switch-Icon-Package", 2);
+user_pref("cmi-Switch-Gecko-Branch", 1);
+user_pref("cmi-Switch-PC-Platform", 2);
