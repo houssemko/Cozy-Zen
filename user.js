b3872dc4849af7b9d49e08e93f886383561de1c6
houssemko <41808005+houssemko@users.noreply.github.com>
2025-11-13 08:40:05 +0100

    Update user.js

--- user.js
+++ user.js
@@ -2,6 +2,44 @@
  Recommended advanced configs                                                       
 **********************************************************************/
 
+/** MOZILLA UI ***/
+user_pref("browser.tabs.groups.enabled",true);
+user_pref("browser.taskbarTabs.enabled",true);
+user_pref("browser.urlbar.weather.featureGate", true);
+user_pref("browser.urlbar.groupLabels.enabled", true);
+user_pref("browser.tabs.allow_transparent_browser", true);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.newtabpage.activity-stream.system.showWeather", true);
+user_pref("browser.preferences.defaultPerformanceSettings.enabled", false);
+user_pref("widget.wayland.opaque-region.enabled",true);
+user_pref("dom.element.animate.enabled", true);
+user_pref("dom.viewTransitions.enabled", true);
+user_pref("accessibility.force_disabled", 1);
+user_pref("browser.uidensity", 1)
+user_pref("browser.privatebrowsing.vpnpromourl", "");
+user_pref("extensions.getAddons.showPane", false);
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+user_pref("browser.discovery.enabled", false);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+user_pref("browser.preferences.moreFromMozilla", false);
+user_pref("browser.tabs.tabmanager.enabled", false);
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.aboutwelcome.enabled", false);
+
+/** THEME ADJUSTMENTS ***/
+user_pref("browser.compactmode.show", true);
+user_pref("browser.display.focus_ring_on_anything", false);
+user_pref("browser.display.focus_ring_style", 0);
+user_pref("browser.display.focus_ring_width", 0);
+user_pref("ui.systemUsesDarkTheme", 1);
+user_pref("layout.css.prefers-color-scheme.content-override", 0);
+user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
+user_pref("screenshots.browser.component.enabled", true);
+user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
+
 /**Zen-UI***/
 user_pref("zen.view.experimental-force-window-controls-left", true);
 user_pref("zen.workspaces.show-workspace-indicator", false);
